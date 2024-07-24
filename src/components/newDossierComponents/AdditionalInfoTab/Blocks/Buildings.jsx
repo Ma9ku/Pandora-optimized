717823baed0ecfa0020e1cafb1f5503c263e671f
AdilSE-2112 <97591283+AdilSE-2112@users.noreply.github.com>
2024-07-24 20:58:44 +0500

    F

--- src/components/newDossierComponents/AdditionalInfoTab/Blocks/Buildings.jsx
+++ src/components/newDossierComponents/AdditionalInfoTab/Blocks/Buildings.jsx
@@ -6,35 +6,25 @@ import BigCollapsableBlock from "../../BigCollapsableBlock";
 import { PiHouseLine } from "react-icons/pi";
 import ActionButton from "../../UI/ActionButton";
 
-function Buildings({
-    data
-}) {
-
+function Buildings({ data }) {
     const [actuals, setActuals] = useState([]);
     const [history, setHistory] = useState([]);
 
     useEffect(() => {
         console.log(data);
 
-        setActuals(prev => {
+        setActuals(() => {
             if (data === null || data.length === 0) return [];
+            return data.filter(item => item.register_status_rus === 'актуальная');
+        });
 
-            return data.filter(item => {
-                if (item.register_status_rus === 'актуальная') return true;
-            })
-        })
-
-        setHistory(prev => {
+        setHistory(() => {
             if (data === null || data.length === 0) return [];
+            return data.filter(item => item.register_status_rus !== 'актуальная');
+        });
+    }, [data]);
 
-            return data.filter(item => {
-                if (item.register_status_rus !== 'актуальная') return true;
-            })
-        })
-    }, [])
-
-    return ( 
-
+    return (
         <BigCollapsableBlock 
             icon={<PiHouseLine />}
             name={'СВЕДЕНИЯ ПО РЕЕСТРУ НЕДВИЖИМОСТИ'}
@@ -43,88 +33,80 @@ function Buildings({
                 name={`Текущие (${actuals.length})`}
             >
                 {console.log(actuals)}
-                {
-                    actuals.map((item, index) => {
-
-                        console.log(item);
-
-                        return (
-                            <CollapsableContainer
-                                key={index}
-                                name={`${index+1}. ${item.intended_use_rus}, Cтатус: "${item.register_status_rus}", ${item.register_reg_date.substring(0, 10)} -`}
-                            >
-                                <VerticalTable 
-                                    data={{
-                                        'Кадастровый номер №': item.cadastral_number,
-                                        'Адрес': item.address_history_rus,
-                                        'Правообладатель': item.owner_full_name,
-                                        'Этажность': item.floor,
-                                        'Площадь общая': item.area_total,
-                                        'Жилая площадь': item.area_useful,
-                                        'Вид документа': item.register_emergence_rights_rus,
-                                        'Номер документа': item.register_record_number,
-                                        'Дата документа': 'NETU',
-                                        'Дата регистрации': item.register_reg_date?.substring(0, 10),
-                                        'Дата прекращения': item.register_end_date?.substring(0, 10) || '---',
-                                        'ИИН/БИН продавца': item.owner_iin_bin,
-                                        'ФИО/Наименование продавца': 'NETU',
-                                        'Сумма сделки (стоимость)': item.register_transaction_amount,
-                                    }}
+                {actuals.map((item, index) => {
+                    console.log(item);
+                    return (
+                        <CollapsableContainer
+                            key={index}
+                            name={`${index + 1}. ${item.intended_use_rus}, Cтатус: "${item.register_status_rus}", ${item.register_reg_date.substring(0, 10)} - ${item.register_end_date ? item.register_end_date.substring(0, 10) : ' '}`}
+                        >
+                            <VerticalTable 
+                                data={{
+                                    'Кадастровый номер №': item.cadastral_number,
+                                    'Адрес': item.address_history_rus,
+                                    'Правообладатель': item.owner_full_name,
+                                    'Этажность': item.floor,
+                                    'Площадь общая': item.area_total,
+                                    'Жилая площадь': item.area_useful,
+                                    'Вид документа': item.register_emergence_rights_rus,
+                                    'Номер документа': item.register_record_number,
+                                    'Дата документа': 'NETU',
+                                    'Дата регистрации': item.register_reg_date?.substring(0, 10),
+                                    'Дата прекращения': item.register_end_date?.substring(0, 10) || '---',
+                                    'ИИН/БИН продавца': item.owner_iin_bin,
+                                    'ФИО/Наименование продавца': 'NETU',
+                                    'Сумма сделки (стоимость)': item.register_transaction_amount,
+                                }}
+                            />
+                            <div className="actions">
+                                <ActionButton 
+                                    onClick={() => {}}
+                                    value={'Детальный просмотр (Купил-Продал)'}
                                 />
-                                <div className="actions">
-                                    <ActionButton 
-                                        onClick={() => {}}
-                                        value={'Детальный просмотр (Купил-Продал)'}
-                                    />
-                                </div>
-                            </CollapsableContainer>
-                        )
-                    })
-                }
+                            </div>
+                        </CollapsableContainer>
+                    );
+                })}
             </SmallCollapsableBlock>
 
             <SmallCollapsableBlock 
                 name={`Исторические (${history.length})`}
             >
-                {
-                    history.map((item, index) => {
-
-                        return (
-                            <CollapsableContainer
-                                key={index}
-                                name={`${index+1}. ${item.intended_use_rus}, Cтатус: "${item.register_status_rus}", ${item.register_reg_date.substring(0, 10)} -`}
-                            >
-                                <VerticalTable 
-                                    data={{
-                                        'Кадастровый номер №': item.cadastral_number,
-                                        'Адрес': item.address_history_rus,
-                                        'Правообладатель': item.owner_full_name,
-                                        'Этажность': item.floor,
-                                        'Площадь общая': item.area_total,
-                                        'Жилая площадь': item.area_useful,
-                                        'Вид документа': item.register_emergence_rights_rus,
-                                        'Номер документа': item.register_record_number,
-                                        'Дата документа': 'NETU',
-                                        'Дата регистрации': item.register_reg_date?.substring(0, 10),
-                                        'Дата прекращения': item.register_end_date?.substring(0, 10) || '---',
-                                        'ИИН/БИН продавца': item.owner_iin_bin,
-                                        'ФИО/Наименование продавца': 'NETU',
-                                        'Сумма сделки (стоимость)': item.register_transaction_amount,
-                                    }}
+                {history.map((item, index) => {
+                    return (
+                        <CollapsableContainer
+                            key={index}
+                            name={`${index + 1}. ${item.intended_use_rus}, Cтатус: "${item.register_status_rus}", ${item.register_reg_date.substring(0, 10)} - ${item.register_end_date ? item.register_end_date.substring(0, 10) : ' '}`}
+                        >
+                            <VerticalTable 
+                                data={{
+                                    'Кадастровый номер №': item.cadastral_number,
+                                    'Адрес': item.address_history_rus,
+                                    'Правообладатель': item.owner_full_name,
+                                    'Этажность': item.floor,
+                                    'Площадь общая': item.area_total,
+                                    'Жилая площадь': item.area_useful,
+                                    'Вид документа': item.register_emergence_rights_rus,
+                                    'Номер документа': item.register_record_number,
+                                    'Дата документа': 'NETU',
+                                    'Дата регистрации': item.register_reg_date?.substring(0, 10),
+                                    'Дата прекращения': item.register_end_date?.substring(0, 10) || '---',
+                                    'ИИН/БИН продавца': item.owner_iin_bin,
+                                    'ФИО/Наименование продавца': 'NETU',
+                                    'Сумма сделки (стоимость)': item.register_transaction_amount,
+                                }}
+                            />
+                            <div className="actions">
+                                <ActionButton 
+                                    onClick={() => {}}
+                                    value={'Детальный просмотр (Купил-Продал)'}
                                 />
-                                <div className="actions">
-                                    <ActionButton 
-                                        onClick={() => {}}
-                                        value={'Детальный просмотр (Купил-Продал)'}
-                                    />
-                                </div>
-                            </CollapsableContainer>
-                        )
-                    })
-                }
+                            </div>
+                        </CollapsableContainer>
+                    );
+                })}
             </SmallCollapsableBlock>
         </BigCollapsableBlock>
-
     );
 }
 
